a0cff69558edd8d7bc3fb1a2f2cae47945b8aa29
birowo <1340zxcvbnm@gmail.com>
2026-06-02 16:14:16 +0700

    commit

--- httpreq.go
+++ httpreq.go
@@ -23,39 +23,38 @@ var (
 	clKey         = []byte(clKStr)
 )
 
-type KeyVal struct {
-	K, V []byte
+type kv struct {
+	Key, Val []byte
 }
 
 type request struct {
 	Method, Path, Proto []byte
-	Headers             [maxHdrsNum]KeyVal
+	Headers             [maxHdrsNum]kv
 	HdrsNum             int
 	//ContentLen          int
 	Body []byte
 }
 
 // func Parse Http Request memproses buffer secara zero-alloc.
-// Mengembalikan (bytesProcessed, incomplete, err).
+// Mengembalikan (request, consumed, incomplete, error).
 // Jika incomplete == true, artinya data belum lengkap (incomplete), gnet harus menunggu data baru.
 func Parse(buf []byte) (req request, reqLen int, incomplete bool, err error) {
 	// 1. Cari batas akhir seluruh hdrs (\r\n\r\n)
-	hdrEnd := bytes.Index(buf, rnrn)
-	if hdrEnd == -1 {
+	hdrLen := bytes.Index(buf, rnrn)
+	if hdrLen == -1 {
 		incomplete = true
 		return // Incomplete data
 	}
-	reqLen = hdrEnd + rnrnLen
-	hdrBuf := buf[:reqLen]
+	hdrEnd := hdrLen + rnrnLen
+	reqLen = hdrEnd
 
 	// 2. Cari Content-Length (Pasti Title-Case karena dari cloudflared tunnel)
-	clIdx := bytes.Index(hdrBuf, clKey)
+	clIdx := bytes.Index(buf[:hdrLen], clKey)
 	if clIdx != -1 {
 		bgn := clIdx + clKeyLen
-		cl := parseUintBytes(hdrBuf[bgn : bytes.IndexByte(hdrBuf[bgn:], rn)+bgn])
+		cl := parseUintBytes(buf[bgn : bgn+bytes.IndexByte(buf[bgn:hdrEnd], rn)])
 
 		// Pastikan seluruh Body sudah masuk di buffer gnet
-		bgn = reqLen
 		reqLen += cl
 		if len(buf) < reqLen {
 			incomplete = true
@@ -63,53 +62,58 @@ func Parse(buf []byte) (req request, reqLen int, incomplete bool, err error) {
 		}
 		if cl > 0 {
 			//req.ContentLen = cl
-			req.Body = buf[bgn:reqLen]
+			req.Body = buf[hdrEnd:reqLen]
 		}
 	}
 
 	// 3. Parsing Request Line
 
 	// Method
-	reqLineEnd := bytes.IndexByte(hdrBuf, rn)
-	reqLine := hdrBuf[:reqLineEnd]
-	sp1 := bytes.IndexByte(reqLine, ' ')
+	sp1 := bytes.IndexByte(buf[:hdrLen], ' ')
 	if sp1 == -1 {
 		err = ErrBadRequest
 		return
 	}
-	req.Method = reqLine[:sp1]
+	req.Method = buf[:sp1]
+	//println(string(buf[:sp1]))
 
 	// Path
 	sp1++ //skip ' '
-	sp2 := bytes.IndexByte(
-		reqLine[sp1:], ' ',
-	)
+	sp2 := bytes.IndexByte(buf[sp1:hdrLen], ' ')
 	if sp2 == -1 {
 		err = ErrBadRequest
 		return
 	}
 	sp2 += sp1
-	req.Path = reqLine[sp1:sp2]
+	req.Path = buf[sp1:sp2]
+	//println(string(buf[sp1:sp2]))
 
 	// Protocol
 	sp2++ //skip ' '
-	req.Proto = reqLine[sp2:]
+	reqLineEnd := bytes.IndexByte(buf[sp2:hdrEnd], rn) + sp2
+	req.Proto = buf[sp2:reqLineEnd]
+	//println(string(buf[sp2:reqLineEnd]))
 
 	// 4. Parsing Seluruh Headers (Key otomatis Title-Case karena dari cloudflared tunnel)
-	bgn := reqLineEnd + rnLen
+	kBgn := reqLineEnd + rnLen
 	hdrIdx := 0
-	for bgn < hdrEnd && hdrIdx < maxHdrsNum {
-		end := bytes.IndexByte(hdrBuf[bgn:], rn) + bgn
-		colonIdx := bytes.IndexByte(hdrBuf[bgn:end], hdrKVSparatr)
-		if colonIdx != -1 {
-			colonIdx += bgn
-			req.Headers[hdrIdx] = KeyVal{
-				hdrBuf[bgn:colonIdx],
-				hdrBuf[colonIdx+hdrKVSparatrLen : end],
-			}
-			hdrIdx++
+	var vEnd int
+	for kBgn < hdrLen && hdrIdx < maxHdrsNum {
+		kEnd := bytes.IndexByte(buf[kBgn:hdrLen], hdrKVSparatr)
+		if kEnd == -1 {
+			err = ErrBadRequest
+			return
 		}
-		bgn = end + rnLen
+		kEnd += kBgn
+		vBgn := kEnd + hdrKVSparatrLen
+		vEnd = bytes.IndexByte(buf[vBgn:hdrEnd], rn) + vBgn
+		//println("k:", string(buf[kBgn:kEnd]), ",v:", string(buf[vBgn:vEnd]))
+		req.Headers[hdrIdx] = kv{
+			Key: buf[kBgn:kEnd],
+			Val: buf[vBgn:vEnd],
+		}
+		hdrIdx++
+		kBgn = vEnd + rnLen
 	}
 	req.HdrsNum = hdrIdx
 	return
--- main/main.go
+++ main/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/birowo/httpreq"
-
 	"github.com/panjf2000/gnet/v2"
 )
 
@@ -11,8 +10,11 @@ type httpServer struct {
 	gnet.BuiltinEventEngine
 }
 
-var badReqRes = []byte(
-	"HTTP/1.1 400 Bad Request\r\nConnection: close\r\n\r\n",
+var (
+	badReqRes = []byte(
+		"HTTP/1.1 400 Bad Request\r\nConnection: close\r\n\r\n",
+	)
+	pongRes = []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 5\r\n\r\npong\n")
 )
 
 func (hs *httpServer) OnTraffic(c gnet.Conn) gnet.Action {
@@ -36,7 +38,7 @@ func (hs *httpServer) OnTraffic(c gnet.Conn) gnet.Action {
 	// --- LOGIKA BISNIS (Eksekusi sebelum c.Discard) ---
 
 	// Kirim response balik ke client
-	c.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 5\r\n\r\npong\n"))
+	c.Write(pongRes)
 
 	println()
 	println("sebelum di-parse:\n", string(buf[:consumed]))
@@ -44,7 +46,7 @@ func (hs *httpServer) OnTraffic(c gnet.Conn) gnet.Action {
 	println("setelah di-parse:")
 	println("method:", string(req.Method), "\npath:", string(req.Path), "\nproto:", string(req.Proto))
 	for _, hdr := range req.Headers[:req.HdrsNum] {
-		println(string(hdr.K), ":", string(hdr.V))
+		println(string(hdr.Key), ":", string(hdr.Val))
 	}
 	if req.Body != nil {
 		println("body:", string(req.Body))
